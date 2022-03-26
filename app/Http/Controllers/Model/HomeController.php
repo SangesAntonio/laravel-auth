3ddbeef9ceeb9c4340f6280162b2901a67de2bde
SangesAntonio <sanges65@gmail.com>
2022-03-26 13:50:15 +0100

    created route middlware

---
+++ app/Http/Controllers/Model/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Model;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.home');
+    }
+}
