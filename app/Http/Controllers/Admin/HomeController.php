5abe0158cc221844e7b4edc20c2fd93345623ea6
SangesAntonio <sanges65@gmail.com>
2022-03-26 17:46:31 +0100

    added route Any

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\Model;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
     /**
      * Show the application dashboard.
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::middleware('auth')
     ->prefix('admin')
@@ -27,3 +24,7 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('posts', 'PostController');
     });
+
+Route::get('{any}', function () {
+    return view('guest.home');
+})->where('any', '.*');
